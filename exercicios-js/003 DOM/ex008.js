824d8675b77dc6fe1fb57195c828838d6661fd12
Richard Oliveira <richard.097@hotmail.com>
2022-09-21 15:27:22 -0300

    aula 10

---
+++ exercicios-js/003 DOM/ex008.js
@@ -0,0 +1,8 @@
+var vel = 50
+if (vel > 60) {
+    console.log(`Sua velocidadde foi de ${vel}, MULTADO!`)
+}
+else {
+    console.log(`Sua velocida é de ${vel}, está dentro do permitido.`)
+}
+console.log('Dirja sempre com cinto de segurança!')
